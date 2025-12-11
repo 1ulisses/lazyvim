108b9eec9991aa0fde82eaeb5e857e4a8a65dcfe
srchby <leandrocdefarias@proton.me>
2025-12-11 10:07:32 -0300

    nord colorscheme

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,16 @@
 return {
   { "mfussenegger/nvim-jdtls" },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "java-debug-adapter",
+        "java-test",
+      },
+    },
+  },
 }
